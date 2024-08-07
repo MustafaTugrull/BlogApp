f8591c0f0548f97ff466ef107af703f138adfc09
mustafatugrul <mustafatugrul034@gmail.com>
2024-08-07 19:20:38 +0300

    Add project files.

---
+++ BlogApiDemo/DataAccesslayer/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccesslayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-AO5K0VF;database=CoreBlogApiDb; integrated security=true;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
